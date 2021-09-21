c7fa23ae72d718e6af0708b15210ebd6c0ead416
Huy <nguyenhuuhuy203@gmail.com>
2021-09-21 12:52:37 +0700

    feat: implement add, delete function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTasks from "./components/AddTasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "The Big Short",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Margin Call",
+      day: "Sep 11th at 4:20pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Shopee Sale",
+      day: "Mar 20th at 0:90pm",
+      reminder: false,
+    },
+  ]);
+
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  // Add task
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 1000) + 1;
+    console.log(id);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Task tracker" />
+      <AddTasks onAdd={addTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No task to show"
+      )}
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+const Button = ({text, color, onClick}) => {
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      className="btn"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+  text: "default",
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
