f6656bb7c590ba0d30a441224fb324f85f900634
Huy <nguyenhuuhuy203@gmail.com>
2021-09-21 18:46:29 +0700

    Feat: Support JSON API, Routing

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,11 +3,11 @@ import { FaTimes } from "react-icons/fa";
 const Task = ({ task, onDelete, onToggle }) => {
   return (
     <div
-      className={`task ${task.reminder ? "reminder" : ""}`}
+      className={`task ${task.reminder && "reminder"}`}
       onDoubleClick={() => onToggle(task.id)}
     >
       <h3>
-        {task.text}
+        {task.text}{" "}
         <FaTimes
           style={{ color: "red", cursor: "pointer" }}
           onClick={() => onDelete(task.id)}
